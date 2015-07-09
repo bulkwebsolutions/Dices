63b4103020a9e8980553d07630d528807c66a2b2
Alex Knows <cruzbiz11@gmail.com>
2015-07-08 21:12:54 -0400

    Initial Commit

---
+++ Dices/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dices
+//
+//  Created by Alex Cruz on 7/8/15.
+//  Copyright (c) 2015 Alex Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Dices/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dices
+//
+//  Created by Alex Cruz on 7/8/15.
+//  Copyright (c) 2015 Alex Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
